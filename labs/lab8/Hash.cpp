b7a23e8f5fdd3f6378197446bc98713724008adc
JordanLevin <jlevin38@binghamton.edu>
2017-04-26 19:02:52 -0400

    fixed lab8 to loop around during collisions hopefully

--- labs/lab8/Hash.cpp
+++ labs/lab8/Hash.cpp
@@ -14,30 +14,58 @@ bool Hash::insert(std::string key, std::string value){
     }
 
     int offset = 0;
-    while(storage[hasher(key)+offset].first != ""){
+    while(storage[hasher(key)+offset].first != "" && hasher(key)+offset < max_size){
         offset++;
     }
-    std::pair<std::string, std::string> p(key, value);
-    storage[hasher(key)+offset] = p;
+    if(storage[hasher(key)+offset].first == ""){
+        std::pair<std::string, std::string> p(key, value);
+        storage[hasher(key)+offset] = p;
+    }
+    for (int i = 0; i < hasher(key); ++i) {
+        if(storage[i].first == ""){
+            std::pair<std::string, std::string> p(key, value);
+            storage[hasher(key)+offset] = p;
+            return true;
+        }
+    } 
     return true;
 }
 
-//fix to account for collisions later
 bool Hash::remove(std::string key){
+    int offset = 0;
     if(storage[hasher(key)].first == "")
         return false;
-    storage[hasher(key)].first = "";
-    storage[hasher(key)].second = "";
-    return true;
+    while(storage[hasher(key) + offset].first != key && hasher(key) + offset < max_size){
+        offset++;
+    }
+    if(storage[hasher(key) + offset].first == key){
+        storage[hasher(key) + offset].first = "";
+        storage[hasher(key) + offset].second = "";
+        return true;
+    }
+    for (int i = 0; i < hasher(key); ++i) {
+        if(storage[i].first == key){
+            storage[i].first = "";
+            storage[i].second = "";
+            return true;
+        }
+    } 
+    
+    return false;
 }
 
 std::string Hash::find(std::string key){
     int index = hasher(key);
-    while(storage[index].first != key && index < size()){
+    while(storage[index].first != key && index < size()-1){
         index++;
     }
     if(storage[index].first == key)
         return storage[index].second;
+    for (int i = 0; i < hasher(key); ++i) {
+        if(storage[i].first == key){
+            return storage[i].second;
+        }
+    } 
     return "";
 }
 
@@ -53,7 +81,7 @@ int Hash::size(){
 
 void Hash::printHash(){
     for(int i = 0;i<size();i++){
-        std::cout<<"Key: " << storage[i].first << ", Value: "<<storage[i].second<<"\n";
+        std::cout<<"Index: " << i << ", Key: " << storage[i].first << ", Value: "<<storage[i].second<<"\n";
     }
 }
 
